aafa3d86f3b4f83c2a871ca2d812171dc8ef2d64
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2020-08-28 18:41:04 +0200

    .net core

--- AdsRemote/AdsRemote/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyProduct("AdsRemote")]
-[assembly: AssemblyTitle("High-level interface for TwinCAT.Ads API")]
-[assembly: AssemblyCompany("Nikolai Voronin")]
-[assembly: AssemblyCopyright("Copyright © Nikolai Voronin 2013-2018")]
-[assembly: AssemblyVersion("0.5.2")]
